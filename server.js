36526c6b8babb40dccc4bfb6217df59625a68977
jose caracas <josecaracas@gmail.com>
2025-06-03 19:06:37 -0400

    galeria actualizada para mostrar imagenes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 // Ruta de prueba
 app.get("/", (req, res) => {
@@ -38,6 +39,8 @@ const galeriaRoutes = require('./routes/galeriaRoutes'); // Importa las rutas de
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 app.use("/api/usuarios", userRoutes);
 app.use("/api/eventos", eventRoutes);
